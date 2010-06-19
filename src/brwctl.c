27a09df94cbae651dd0e5fc838cb2b764d3e32e4
Henning Bekel <h.bekel@googlemail.com>
2010-06-19 20:36:12 +0200

    Brwctl: Code cleanup: added header file.

--- src/brwctl.c
+++ src/brwctl.c
@@ -1,3 +1,5 @@
+/* brwctl.c: simple window control utility for bashrun */
+
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <X11/Xresource.h>
@@ -7,32 +9,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
-
-#define _NET_WM_STATE_REMOVE 0
-#define _NET_WM_STATE_ADD 1
-#define _NET_WM_STATE_TOGGLE 2
-
-static int map();
-static int unmap();
-static int is_mapped();
-static int raise();
-static int focus();
-static int has_focus();
-static int activate();
-static int current();
-static int show();
-static int pos(int, int);
-static int size(int, int, int);
-static int get_class();
-static int search_window(char*, Window);
-static int message(char*, unsigned long, unsigned long, unsigned long, unsigned long, unsigned long);
-static unsigned char *property(Window, Atom, long*, Atom*, int*);
-static int bad_window(Display*, XErrorEvent*);
-
-Display *dpy;
-Window win;
-char *classname;
-Window root;
+#include <brwctl.h>
 
 int main(int argc, char **argv)
 { 
---
+++ src/brwctl.h
@@ -0,0 +1,25 @@
+#define _NET_WM_STATE_REMOVE 0
+#define _NET_WM_STATE_ADD 1
+#define _NET_WM_STATE_TOGGLE 2
+
+static int map();
+static int unmap();
+static int is_mapped();
+static int raise();
+static int focus();
+static int has_focus();
+static int activate();
+static int current();
+static int show();
+static int pos(int, int);
+static int size(int, int, int);
+static int get_class();
+static int search_window(char*, Window);
+static int message(char*, unsigned long, unsigned long, unsigned long, unsigned long, unsigned long);
+static unsigned char *property(Window, Atom, long*, Atom*, int*);
+static int bad_window(Display*, XErrorEvent*);
+
+Display *dpy;
+Window win;
+char *classname;
+Window root;
